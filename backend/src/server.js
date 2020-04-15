2ca14aae4c28ab270cd0b6ffe535bb4070974999
Mateus Bombassaro <mateusbpinho@gmail.com>
2020-04-14 23:18:38 -0300

    criado backend para tindev

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,3 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const routes = require('./routes');
 
 const server = express();
+
+mongoose.connect(
+  'mongodb+srv://tindev:tindev@cluster0-pzx1l.mongodb.net/tindev?retryWrites=true&w=majority',
+  { useUnifiedTopology: true, useNewUrlParser: true },
+);
+
+server.use(cors());
+server.use(express.json());
+server.use(routes);
+
+server.listen(3333);
